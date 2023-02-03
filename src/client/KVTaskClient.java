f6354fbdd9c35202471c8b91ea6015bb61c09ab4
wbryon <hatami1974@yandex.ru>
2023-02-03 23:11:50 +0300

    добавлена реализация конструктора в классе KVTaskClient

--- src/client/KVTaskClient.java
+++ src/client/KVTaskClient.java
@@ -1,16 +1,37 @@
 package client;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 /**
  * Класс KVTaskClient - это HTTP-клиент, который будет делегировать вызовы методов в HTTP-запросы.
  * Его будет использовать класс HttpTaskManager
  */
 
 public class KVTaskClient {
+    private String apiToken;
+    HttpClient client;
+    HttpResponse.BodyHandler<String> handler;
+    HttpResponse<String> response;
     /**
      * Конструктор принимает URL к серверу хранилища и регистрируется.
      * При регистрации выдаётся токен (API_TOKEN), который нужен при работе с сервером.
+     *
+     * Конструктор создаёт объект, описывающий HTTP-запрос
      */
-    public KVTaskClient() {
+    public KVTaskClient(URI uri) throws IOException, InterruptedException {
+        client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()    // указываем HTTP-метод запроса
+                .uri(uri) // указываем адрес ресурса
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "text/html")
+                .build(); // заканчиваем настройку и создаём ("строим") HTTP-запрос
+        handler = HttpResponse.BodyHandlers.ofString(); // получаем стандартный обработчик тела ответа с конвертацией содержимого в строку
+        response = client.send(request, handler);
     }
 
     /**
